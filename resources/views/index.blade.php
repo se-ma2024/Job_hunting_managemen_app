20f84e1966fbf6a8ed5acd887f254eb11ef9b7a9
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-03-04 10:46:45 +0900

    Separate css files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,176 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>就活管理アプリ</title>
-    <link rel="stylesheet" href="{{ asset('/css/companies_list.blade.css') }}">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 10px 20px;
-            display: flex;
-            align-items: center;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 1000;
-        }
-
-        header h1 {
-            margin: 0;
-            padding-right: 100px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        .hamburger {
-            cursor: pointer;
-            margin-right: 20px;
-        }
-
-        .hamburger .row {
-            width: 30px;
-            height: 3px;
-            background-color: #fff;
-            margin: 5px 0;
-        }
-
-        .menu-overlay {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 999;
-            display: none;
-        }
-
-        .menu {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 200px;
-            height: 100%;
-            background-color: #333;
-            z-index: 1001;
-            transform: translateX(-100%);
-            transition: transform 0.3s ease;
-            padding-top: 50px;
-        }
-
-        .menu.active {
-            transform: translateX(0);
-        }
-
-        .menu ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            text-align: center;
-        }
-
-        .menu ul li {
-            padding: 20px 0;
-        }
-
-        .menu ul li a {
-            text-decoration: none;
-            color: #fff;
-            font-size: 18px;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 80px auto 20px;
-            background-color: #f9f9f9;
-            padding: 30px;
-            border-radius: 15px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-        }
-
-        .container h1 {
-            margin: 0;
-            text-align: center;
-            color: #000;
-            margin-bottom: 30px;
-            font-size: 36px;
-        }
-
-        .company-card {
-            background-color: #fff;
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            margin-bottom: 20px;
-            transition: transform 0.3s ease;
-            cursor: pointer;
-        }
-
-        .company-card:hover {
-            transform: translateY(-5px);
-        }
-
-        .company-card .company-name {
-            font-size: 24px;
-        }
-
-        .company-info {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .float-button {
-            position: fixed;
-            bottom: 20px;
-            right: 20px;
-            background-color: #00C000;
-            color: #fff;
-            border: none;
-            border-radius: 50%;
-            width: 50px;
-            height: 50px;
-            font-size: 24px;
-            text-align: center;
-            line-height: 50px;
-            cursor: pointer;
-            z-index: 1002;
-        }
-
-        .float-button:hover {
-            background-color: #0056b3;
-        }
-
-        .container button[type="submit"],
-        a {
-            display: block;
-            width: calc(100%);
-            padding: 10px;
-            box-sizing: border-box;
-            text-align: center;
-            background-color: #333;
-            color: #fff;
-            border: none;
-            border-radius: 5px;
-            text-decoration: none;
-            font-size: 18px;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        }
-
-        .container button[type="submit"]:hover,
-        a:hover {
-            background-color: #555;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('../resources/css/index.css') }}">
 </head>
 <body>
     <header>
@@ -189,7 +20,6 @@
                 <li><a href="{{ route('index') }}">ホーム</a></li>
                 <li><a href="{{ route('showProfile') }}">プロフィール</a></li>
                 <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a></li>
-                <!-- お問い合わせやその他のメニュー項目をここに追加 -->
             </ul>
         </nav>
 
@@ -208,9 +38,14 @@
                 </div>
             </div>
         @endforeach
-            <a href="{{ route('createCompany') }}" class="float-button">＋</a>
+        <div class="speech-bubble">
+            <p>企業を追加する</p>
+        </div>
+        <a href="{{ route('createCompany') }}" class="float-button">＋</a>
         </main>
     </div>
+
+    
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,51 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>就活情報管理アプリケーション</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f9f9f9;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .container {
-            max-width: 800px;
-            margin: -60px auto 20px;
-            background-color: #f9f9f9;
-            padding: 30px;
-            border-radius: 15px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-        h1 {
-            font-size: 36px;
-            color: #333;
-            margin-bottom: 20px;
-        }
-        .btn {
-            display: inline-block;
-            padding: 12px 24px;
-            background-color: #007bff;
-            color: #fff;
-            text-decoration: none;
-            border-radius: 5px;
-            transition: background-color 0.3s ease;
-            font-size: 18px;
-            margin: 10px;
-            cursor: pointer;
-            border: none;
-        }
-        .btn:hover {
-            background-color: #0056b3;
-        }
-        .btn i {
-            margin-right: 10px;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('../resources/css/top.css') }}">
 </head>
 <body>
     <div class="container">
